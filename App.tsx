b761805b5eaaf81771988d8b397f8f4adecc31e5
DanTol-cloud <danielkozac05@gmail.com>
2024-03-25 15:40:58 +0200

    PDFViewer

--- App.tsx
+++ App.tsx
@@ -1,118 +1,103 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { Component } from "react";
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  Platform,
   StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  BackHandler,
+  Alert,
+  TouchableOpacity, Text,
+} from "react-native";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { DocumentView, RNPdftron } from "@pdftron/react-native-pdf";
+import DocumentPicker from 'react-native-document-picker';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+type Props = {};
+type State = {
+  documentPath: string | null;
+};
+export default class App extends Component<Props> {
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  constructor(props: Props) {
+    super(props);
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    this.state = {
+      documentPath: null,
+    } ;
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    RNPdftron.enableJavaScript(true);
+  }
+
+  onLeadingNavButtonPressed = () => {
+    console.log("leading nav button pressed");
+    if (Platform.OS === "ios") {
+      Alert.alert(
+          "App",
+          "onLeadingNavButtonPressed",
+          [{ text: "OK", onPress: () => console.log("OK Pressed") }],
+          { cancelable: true }
+      );
+    } else {
+      BackHandler.exitApp();
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  handleFilePick = async () => {
+    try {
+      const res = await DocumentPicker.pick({
+        type: [DocumentPicker.types.pdf],
+      });
+      this.setState({ documentPath: res[0].uri });
+    } catch (err) {
+      if (DocumentPicker.isCancel(err)) {
+        console.log('User cancelled the file picker');
+      } else {
+        console.error('Error while picking the file', err);
+      }
+    }
+  };
+
+
+  render(){
+
+    const { documentPath } = this.state;
+
+    return (
+        <>
+          <DocumentView
+              ref={(c) => this._viewer = c}
+              hideAnnotationToolbarSwitcher={false}
+              hideTopToolbars={false}
+              hideTopAppNavBar={false}
+              document={documentPath ? documentPath : "https://pdftron.s3.amazonaws.com/downloads/pl/PDFTRON_mobile_about.pdf"}
+              padStatusBar={true}
+              showLeadingNavButton={true}
+              leadingNavButtonIcon={Platform.OS === 'ios' ? 'ic_close_black_24px.png' : 'ic_arrow_back_white_24dp'}
+              onLeadingNavButtonPressed={this.onLeadingNavButtonPressed}
+              readOnly={false}
+              openOutlineList = {true}
+          />
+          <TouchableOpacity style={styles.selectButton} onPress={this.handleFilePick}>
+            <Text style={styles.buttonText}>Select File</Text>
+          </TouchableOpacity>
+        </>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  selectButton: {
+    position: 'absolute',
+    bottom: 50,
+    right: 20,
+    borderRadius: 50,
+    backgroundColor: '#52b8f7',
+    padding: 10,
+    zIndex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
-
-export default App;
